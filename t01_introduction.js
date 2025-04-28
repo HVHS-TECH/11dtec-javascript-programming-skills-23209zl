93a8d9690877faaf626047c355222d39397cdab9
23209zl <23209zl@hvhs.school.nz>
2025-04-28 21:59:20 +0000

    I made a change

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,4 +1,17 @@
-
-
-
+/*************************
+Name of task: Introduction to Javascript
+Author: Jackson
+Date: 29/04/25
+*************************/
 console.log("Running t01_introduction.js");
+
+//Variables
+
+/**************************
+Main code
+*************************/
+console.log("Hello");
+
+/**************************
+Functions
+*************************/
