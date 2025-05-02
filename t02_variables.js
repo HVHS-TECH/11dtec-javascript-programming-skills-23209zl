5c7432164049e940665826fe195948a2c8f450fe
23209zl <23209zl@hvhs.school.nz>
2025-05-02 02:38:13 +0000

    May 2, 2025, 2:38 AM

--- t02_variables.js
+++ t02_variables.js
@@ -7,13 +7,17 @@ console.log("Running t02_variables.js");
 
 //Variables
 let pocketMoney = 3;
-
+let userName = "Jackson Li"
+let userAge = 15
+let currentYear = 2025
 /**************************
 Main code
 *************************/
-console.log("Hi")
+console.log(userName)
+console.log(userAge)
+console.log(currentYear)
 console.log("You have ",pocketMoney, " dollars");
-
+console.log("Hi")
 /**************************
 Functions
 *************************/
