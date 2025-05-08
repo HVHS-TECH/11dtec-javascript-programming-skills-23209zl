a65c96a1b8b15ed630586c0468f88ada20bed0e9
23209zl <23209zl@hvhs.school.nz>
2025-05-08 01:04:47 +0000

    May 8, 2025, 1:04 AM

---
+++ t05_alerts_prompts.js
@@ -0,0 +1,17 @@
+/*************************
+Name of task: Alerts and prompts
+Author: Jackson
+Date: 8/05/25
+*************************/
+console.log("Running t05_alerts_prompts.js");
+
+//Variables
+let userName
+/**************************
+Main code
+*************************/
+userName = prompt('What is your name?');
+console.log(userName);
+/**************************
+Functions
+*************************/
