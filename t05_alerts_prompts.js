2bac0dc6d72591a699ce5bcaef023ec6492991f2
23209zl <23209zl@hvhs.school.nz>
2025-05-08 01:20:25 +0000

    May 8, 2025, 1:20 AM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -6,12 +6,12 @@ Date: 8/05/25
 console.log("Running t05_alerts_prompts.js");
 
 //Variables
-let userName
 
 /**************************
 Main code
 *************************/
-prompt("Welcome to my program! \n Please enter your name");
+alert("Welcome to my program!");
+prompt('What is your name?');
 prompt('What is your age?');
 prompt('How much pocket money do you have?');
 
