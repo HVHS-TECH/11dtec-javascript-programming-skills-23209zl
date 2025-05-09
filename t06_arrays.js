1ec7ae04a529bda86a9e1fe8e521fdab2198ad2d
23209zl <23209zl@hvhs.school.nz>
2025-05-09 02:46:05 +0000

    May 9, 2025, 2:46 AM

---
+++ t06_arrays.js
@@ -0,0 +1,23 @@
+/*************************
+Name of task: Alerts and prompts
+Author: Jackson
+Date: 8/05/25
+*************************/
+console.log("Running t05_alerts_prompts.js");
+
+//arrays
+let Chocolate=["I like chocolate", "Chocolate is the best thing EVER"]
+
+//Variables
+let userCHOC
+
+/**************************
+Main code
+*************************/
+
+userCHOC= prompt("Do you like chocolate 0-1")
+alert(Chocolate[userCHOC])
+
+/**************************
+Functions
+*************************/
