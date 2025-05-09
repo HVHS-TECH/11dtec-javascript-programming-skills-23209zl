ab6af05d6ff7866f90943c55f1f2ea543965405d
23209zl <23209zl@hvhs.school.nz>
2025-05-09 02:31:00 +0000

    May 9, 2025, 2:31 AM

--- t03_maths.js
+++ t03_maths.js
@@ -9,14 +9,16 @@ console.log("Running t03_maths.js");
 let userName = "Jackson Li"
 let userAge = 15
 let pocketMoney = 10
+let userBorn = 2009
 /**************************
 Main code
 *************************/
 console.log("Hi" + userName)
-console.log("As of" + userName + "you are" + userAge + "years old")
-console.log("In ten years you will be" + (userAge + 10) + "years old")
+console.log("As of" + userName + "you are" + userAge + "years old");
+console.log("you are born in" + userBorn);
+console.log("In ten years you will be" + (userAge + 10) + "years old");
 console.log("You have " + pocketMoney + " dollars");
-console.log("Half of your money is" + pocketMoney/2)
+console.log("Half of your money is" + pocketMoney/2);
 /**************************
 Functions
 *************************/
--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -7,6 +7,9 @@ console.log("Running t05_alerts_prompts.js");
 
 //Variables
 
+let userName = "Jackson Li"
+let userAge = 15
+let userBorn = 2009
 /**************************
 Main code
 *************************/
@@ -14,8 +17,12 @@ alert("Welcome to my program!");
 prompt('What is your name?');
 prompt('What is your age?');
 prompt('How much pocket money do you have?');
-
-
+alert("Hi" + userName);
+alert("As of" + userName + "you are" + userAge + "years old");
+alert("You were born in" + userBorn);
+alert("In ten years you will be" + (userAge + 10) + "years old");
+alert("You have " + pocketMoney + " dollars");
+alert("Half of your money is" + pocketMoney/2);
 /**************************
 Functions
 *************************/
