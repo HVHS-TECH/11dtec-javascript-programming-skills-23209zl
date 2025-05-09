94c6be0c3c79cec3f3c80fc09fd08711e5e8c5bf
23209zl <23209zl@hvhs.school.nz>
2025-05-09 02:48:20 +0000

    May 9, 2025, 2:48 AM

--- t06_arrays.js
+++ t06_arrays.js
@@ -6,7 +6,7 @@ Date: 8/05/25
 console.log("Running t05_alerts_prompts.js");
 
 //arrays
-let Chocolate=["I like chocolate", "Chocolate is the best thing EVER"]
+let Chocolate=["I don't like chocolate", "I like chocolate", "Chocolate is the best thing EVER"]
 
 //Variables
 let userCHOC
@@ -15,7 +15,7 @@ let userCHOC
 Main code
 *************************/
 
-userCHOC= prompt("Do you like chocolate 0-1")
+userCHOC= prompt("Do you like chocolate 0-2")
 alert(Chocolate[userCHOC])
 
 /**************************
