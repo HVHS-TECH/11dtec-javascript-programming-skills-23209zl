ea74371ae4644b54a923a740e46f1bdade28b393
23209zl <23209zl@hvhs.school.nz>
2025-05-12 21:10:07 +0000

    May 12, 2025, 9:10 PM

---
+++ t07_if.js
@@ -0,0 +1,21 @@
+/*************************
+Name of task: Alerts and prompts
+Author: Jackson
+Date: 13/05/25
+*************************/
+console.log("Running t07_if.js");
+
+//arrays
+
+
+//Variables
+
+
+/**************************
+Main code
+*************************/
+
+
+/**************************
+Functions
+*************************/
