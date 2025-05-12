48bbae1d78dea2ddf69d8ed719516d78665aba72
23209zl <23209zl@hvhs.school.nz>
2025-05-12 21:24:23 +0000

    May 12, 2025, 9:24 PM

--- t07_if.js
+++ t07_if.js
@@ -6,12 +6,12 @@ Date: 13/05/25
 console.log("Running t07_if.js");
 
 //Variables
-
+let pocketMoney
 
 /**************************
 Main code
 *************************/
-
+pocketMoney = prompt("How much money do you have?")
 
 /**************************
 Functions
