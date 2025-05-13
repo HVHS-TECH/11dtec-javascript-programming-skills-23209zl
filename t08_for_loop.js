35ab2a634441a5402b1fda32ea20066ff2c9d8ac
23209zl <23209zl@hvhs.school.nz>
2025-05-13 23:49:44 +0000

    May 13, 2025, 11:49 PM

---
+++ t08_for_loop.js
@@ -0,0 +1,17 @@
+/*************************
+Name of task: Introduction to Javascript
+Author: Jackson
+Date: 14/05/25
+*************************/
+console.log("Running t08_for_loops.js");
+
+//Variables
+
+/**************************
+Main code
+*************************/
+
+
+/**************************
+Functions
+*************************/
