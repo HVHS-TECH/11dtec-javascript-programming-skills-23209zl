767c59273a1f4a4eb4dd1b93a7b0f558485e1f47
23209zl <23209zl@hvhs.school.nz>
2025-05-14 00:08:10 +0000

    May 14, 2025, 12:08 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -3,7 +3,7 @@ Name of task: Iteration - The For Loop
 Author: Jackson
 Date: 14/05/25
 *************************/
-console.log("Running t08_for_loops.js");
+console.log("Running t08_for_loop.js");
 
 //Variables
 
@@ -12,7 +12,11 @@ Main code
 *************************/
 
 //loop from 0 to 9
-
+console.log("starting loop");
+for (count=0; count<10; count ++); {
+    console.log("current count:" + count);
+}
+console.log("loop stoped")
 /**************************
 Functions
 *************************/
