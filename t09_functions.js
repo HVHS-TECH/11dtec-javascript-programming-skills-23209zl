84c8650e839b2f97b56e701d4da4bce8e0b3b6e5
23209zl <23209zl@hvhs.school.nz>
2025-05-15 00:55:44 +0000

    May 15, 2025, 12:55 AM

---
+++ t09_functions.js
@@ -0,0 +1,17 @@
+/*************************
+Name of task: Functions
+Author: Jackson
+Date: 15/05/25
+*************************/
+console.log("Running t09_functions.js");
+
+//Variables
+let 
+/**************************
+Main code
+*************************/
+
+
+/**************************
+Functions
+*************************/
