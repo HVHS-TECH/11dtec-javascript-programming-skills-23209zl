33c3d983e519289a9c6042b9603fb4841dbcf329
23209zl <23209zl@hvhs.school.nz>
2025-05-15 01:27:27 +0000

    I made a change

---
+++ t10_activate_via_button.js
@@ -0,0 +1,16 @@
+/*************************
+Name of task: Activate via button
+Author: Jackson
+Date: 15/05/25
+*************************/
+console.log("Running t10_activate_via_buttons.js");
+
+//Variables
+
+/**************************
+Main code
+*************************/
+
+/**************************
+Functions
+*************************/
