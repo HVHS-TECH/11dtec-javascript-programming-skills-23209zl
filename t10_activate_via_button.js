686762fac5f265b98df71b6dad57d399aa7e27a1
23209zl <23209zl@hvhs.school.nz>
2025-05-16 02:33:16 +0000

    May 16, 2025, 2:33 AM

--- t10_activate_via_button.js
+++ t10_activate_via_button.js
@@ -1,16 +1,37 @@
 /*************************
 Name of task: Activate via button
 Author: Jackson
-Date: 15/05/25
+Date: 16/05/25
 *************************/
-console.log("Running t10_activate_via_buttons.js");
+console.log("Running t10_activate_via_button.js");
 
 //Variables
 
 /**************************
 Main code
 *************************/
-
+function start() {
+    askUSERname();
+    askUSERage();
+    askUSERmoney ();
+}
 /**************************
 Functions
 *************************/
+function askUSERname () {
+askUSERname= prompt("What is your name?");
+alert("Hi " + askUSERname + "!")
+} 
+function askUSERage () {
+askUSERage= prompt("How old are you?");
+alert("You are " + askUSERage + " years old")
+} 
+function askUSERmoney () {
+askUSERmoney= prompt("How much pocket money do you have?");
+if (askUSERmoney < 4){
+alert("You can't afford a bar of chocolate")
+}
+if (askUSERmoney > 4){
+    alert("You can afford a chocolate bar")
+}
+} 
