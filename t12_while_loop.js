cc3807a3afb731c50645de1aee2106addab07d51
23209zl <23209zl@hvhs.school.nz>
2025-05-27 23:32:48 +0000

    May 27, 2025, 11:32 PM

---
+++ t12_while_loop.js
@@ -0,0 +1,17 @@
+/*************************
+Name of task: While loops
+Author: Jackson
+Date: 27/05/25
+*************************/
+console.log("Running T12_while_loop.js");
+
+//Variables
+
+/**************************
+Main code
+*************************/
+
+
+/**************************
+Functions
+*************************/
