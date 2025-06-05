d51f3fd1eb1a11eb8ba72ba6dae03bc67cfcb41c
23209zl <23209zl@hvhs.school.nz>
2025-06-05 01:19:20 +0000

    Jun 5, 2025, 1:19 AM

--- t15_array_sorting.js
+++ t15_array_sorting.js
@@ -15,8 +15,8 @@ Main code
 function start() {
     askUSERname();
     askUSERage();
-    askUSERmoney ();
     alert (message);
+    askUSERmoney ();
 }
 /**************************
 Functions
@@ -44,10 +44,36 @@ function askUSERage () {
 alert("You are " + askUSERage + " years old")
 }
 
+function askUSERmoney () {
+while (askUSERmoneyinvalid == true) {
+        askUSERmoney = prompt("How much pocket money do you have from 1 to 5?");
+        if (askUSERmoney == "" || askUSERmoney == " " || askUSERmoney == null || isNaN(askUSERmoney)) {
+         alert("Error! How much pocket money do you have from 1 to 5")
+        } else {
+            askUSERmoneyinvalid = false;
+        }
+}
+if (askUSERmoney == 1){
+    alert("You can afford a Mars Bar")
+}
+if (askUSERmoney == 2){
+    alert("You can afford a Moro Bar")
+}
+if (askUSERmoney == 3){
+    alert("You can afford a King Size Crunchy")
+}
+if (askUSERmoney == 4){
+    alert("You can afford a Dairy Milk Block")
+}
+if (askUSERmoney == 5){
+    alert("You can afford a Lindt Block")
+}
+}
+
 let menu = ["Mars Bar", "Moro Bar", "King Size Crunchy", "Dairy Milk Block", "Lindt Block"];
 let menuPRICE = ["1", "2", "3", "4", "5"];
 
 let message = "Welcome to my shop, here are a list of things you can buy:"
 for (i = 0; i < menu. length; i++){
-    message = message + "\nFood " + (i + 1) + " is " + menu[i] + " it is " + menuPRICE[i];
+    message = message + "\nFood " + (i + 1) + " is " + menu[i] + " it is $" + menuPRICE[i];
 }
